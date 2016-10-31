81bf3a92dff3df738b6c6490fa148e7ddae2b362
Junior <jhimmayati17@gmail.com>
2016-10-31 23:03:23 +0700

    Livecycle Basic

--- app/src/main/java/id/sch/smktelkom_mlg/learn/activitylivecycle/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/activitylivecycle/MainActivity.java
@@ -1,6 +1,5 @@
 package id.sch.smktelkom_mlg.learn.activitylivecycle;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -8,7 +7,7 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "LivecycleTag";
-    MediaPlayer mediaPlayer;
+
 
 
     @Override
@@ -17,5 +16,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "onCreate: ");
+        @Override
+        protected void onStart () {
+            super.onStart();
+            Log.d(TAG, "onStart: ");
+        }
+
+        @Override
+        protected void onResume () {
+            super.onResume();
+            Log.d(TAG, "onResume: ");
+
+        }
+
+        @Override
+        protected void onPause () {
+            super.onPause();
+            Log.d(TAG, "onPause: ");
+
+        }
+
+        @Override
+        protected void onStop () {
+            super.onStop();
+            Log.d(TAG, "onStop: ");
+        }
+
+        @Override
+        protected void onRestart () {
+            super.onRestart();
+            Log.d(TAG, "onRestart: ");
+        }
+
+        @Override
+        protected void onDestroy () {
+            super.onDestroy();
+            Log.d(TAG, "onDestroy: ");
     }
 }
